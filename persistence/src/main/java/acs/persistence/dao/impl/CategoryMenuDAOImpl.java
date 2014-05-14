8ce5e65cfad2f139aa4c1a2ff8f6b1fa7e40ce5d
ksyuOk <oksana.mazurova964@gmail.com>
2014-05-14 22:27:28 +0300

    Added @Repository.

--- persistence/src/main/java/acs/persistence/dao/impl/CategoryMenuDAOImpl.java
+++ persistence/src/main/java/acs/persistence/dao/impl/CategoryMenuDAOImpl.java
@@ -2,12 +2,14 @@ package acs.persistence.dao.impl;
 
 import acs.persistence.dao.ICategoryMenuDAO;
 import acs.persistence.model.CategoryMenu;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created with IntelliJ IDEA.
  * User: oksana
  * Date: 14.05.14
  */
+@Repository
 public class CategoryMenuDAOImpl extends BaseDao<CategoryMenu> implements ICategoryMenuDAO {
     public CategoryMenuDAOImpl(Class<CategoryMenu> entity) {
         super(entity);
